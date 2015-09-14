30301a6273ddc04d3f05593d36803a35e3decd8c
Len Payne <len.payne@lambtoncollege.ca>
2015-09-14 15:48:32 -0400

    Initial Commit (Answered Question)

---
+++ src/javaassignment1/pkg2015/JavaAssignment12015.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment1.pkg2015;
+
+/**
+ *
+ * @author c0587637
+ */
+public class JavaAssignment12015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World"); 
+        
+        /*****************************************
+         * Assignment #1
+         * 
+         * Job Posting: Java Developer
+         * http://www.indeed.ca/cmp/Tundra-Technical-Solution/jobs/Back-End-Java-Developer-be6b886816880859?q=java
+         * 
+         * Java EE - Java Enterprise Edition - https://en.wikipedia.org/wiki/Java_Platform,_Enterprise_Edition
+         * Spring Framework - Application Framework - https://en.wikipedia.org/wiki/Spring_Framework
+         * PIG - High Level Map Reduce - https://en.wikipedia.org/wiki/Pig_(programming_tool)
+         * 
+         * Toronto Salary - $78k
+         * Vancouver Salary - $82k
+         * Waterloo Salary - $80k
+         * 
+         */
+                
+    }
+    
+}
